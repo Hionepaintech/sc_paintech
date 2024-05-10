d044f088ba25dfa877134fdc77b1f8b4c70aa990
Paintech <163475046+Hionepaintech@users.noreply.github.com>
2024-05-11 03:27:53 +0700

    Add files via upload

--- slowdns/installsl.sh
+++ slowdns/installsl.sh
@@ -1,4 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+#setting IPtables
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+netfilter-persistent save
+netfilter-persistent reload
+
+cd
+#delete directory
+rm -rf /etc/xray/dns
+rm xray/dns
+
 ns_domain_cloudflare() {
 	DOMAIN="vip-server.cloud"
 	DOMAIN_PATH=$(cat /etc/xray/domain)
@@ -44,21 +55,115 @@ ns_domain_cloudflare() {
 	echo $NS_DOMAIN >/etc/xray/dns
 }
 
-setup_dnstt() {
-	cd
-	mkdir -p /etc/slowdns
-	wget -O dnstt-server "https://raw.githubusercontent.com/Hionepaintech/sc_paintech/main/slowdns/dnstt-server" >/dev/null 2>&1
-	chmod +x dnstt-server >/dev/null 2>&1
-	wget -O dnstt-client "https://raw.githubusercontent.com/Hionepaintech/sc_paintech/main/slowdns/dnstt-client" >/dev/null 2>&1
-	chmod +x dnstt-client >/dev/null 2>&1
-	./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
-	chmod +x *
-	mv * /etc/slowdns
-	wget -O /etc/systemd/system/client.service "https://raw.githubusercontent.com/Hionepaintech/sc_paintech/main/slowdns/client" >/dev/null 2>&1
-	wget -O /etc/systemd/system/server.service "https://raw.githubusercontent.com/Hionepaintech/sc_paintech/main/slowdns/server" >/dev/null 2>&1
-	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/client.service 
-	sed -i "s/xxxx/$NS_DOMAIN/g" /etc/systemd/system/server.service 
-}
+nameserver=$(cat /etc/xray/dns)
+apt update -y
+apt install -y python3 python3-dnslib net-tools
+apt install ncurses-utils -y
+apt install dnsutils -y
+#apt install golang -y
+apt install git -y
+apt install curl -y
+apt install wget -y
+apt install ncurses-utils -y
+apt install screen -y
+apt install cron -y
+apt install iptables -y
+apt install -y git screen whois dropbear wget
+#apt install -y pwgen python php jq curl
+apt install -y sudo gnutls-bin
+#apt install -y mlocate dh-make libaudit-dev build-essential
+apt install -y dos2unix debconf-utils
+service cron reload
+service cron restart
+
+#tambahan port openssh
+cd
+echo "Port 2222" >> /etc/ssh/sshd_config
+echo "Port 2269" >> /etc/ssh/sshd_config
+sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
+service ssh restart
+service sshd restart
+
+#konfigurasi slowdns
+rm -rf /etc/slowdns
+mkdir -m 777 /etc/slowdns
+wget -q -O /etc/slowdns/server.key "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server.key"
+wget -q -O /etc/slowdns/server.pub "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server.pub"
+wget -q -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/sldns-server"
+wget -q -O /etc/slowdns/sldns-client "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/sldns-client"
+cd
+chmod +x /etc/slowdns/server.key
+chmod +x /etc/slowdns/server.pub
+chmod +x /etc/slowdns/sldns-server
+chmod +x /etc/slowdns/sldns-client
+
+cd
+#wget -q -O /etc/systemd/system/client-sldns.service "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/client-sldns.service"
+#wget -q -O /etc/systemd/system/server-sldns.service "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/server-sldns.service"
+
+cd
+#install client-sldns.service
+cat > /etc/systemd/system/client-sldns.service << END
+[Unit]
+Description=Client SlowDNS By HideSSH
+Documentation=https://hidessh.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/etc/slowdns/sldns-client -udp 8.8.8.8:53 --pubkey-file /etc/slowdns/server.pub $nameserver 127.0.0.1:2222
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+cd
+#install server-sldns.service
+cat > /etc/systemd/system/server-sldns.service << END
+[Unit]
+Description=Server SlowDNS By HideSSH
+Documentation=https://hidessh.com
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /etc/slowdns/server.key $nameserver 127.0.0.1:2269
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+#permission service slowdns
+cd
+chmod +x /etc/systemd/system/client-sldns.service
+
+chmod +x /etc/systemd/system/server-sldns.service
+pkill sldns-server
+pkill sldns-client
+
+systemctl daemon-reload
+systemctl stop client-sldns
+systemctl stop server-sldns
+
+systemctl enable client-sldns
+systemctl enable server-sldns
+
+systemctl start client-sldns
+systemctl start server-sldns
+
+systemctl restart client-sldns
+systemctl restart server-sldns
+
 ns_domain_cloudflare
-setup_dnstt
+
 exit
