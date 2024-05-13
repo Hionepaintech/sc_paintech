2550616176331c7b6cf115a78af3c74f459f9b2b
Paintech <163475046+Hionepaintech@users.noreply.github.com>
2024-05-14 00:09:36 +0700

    Add files via upload

--- menu/menu.sh
+++ menu/menu.sh
@@ -26,11 +26,8 @@ ipsaya=$(wget -qO- ifconfig.me)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
 data_ip="https://raw.githubusercontent.com/Hionepaintech/Licensing-/main/ipmini"
-sudo apt-get update
-sudo apt-get install sysstat
-cpu_usage=$(mpstat 1 1 | awk '$12 ~ /[0-9.]+/ {print $12}')
-cpu_usage=$(echo $cpu_usage | tr -d '\n')
 cpu_load=$(uptime | awk -F'load average:' '{print $2}' | cut -d, -f1)
+cpu_load_percent=$(echo "scale=2; $cpu_load * 100 / $(nproc)" | bc)
 checking_sc() {
 useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
 if [[ $date_list < $useexp ]]; then
@@ -426,8 +423,7 @@ echo -e " $COLOR1│$NC${WH} ❈ UPTIME        ${COLOR1}: ${WH}$uphours $upminut
 echo -e " $COLOR1│$NC${WH} ❈ ISP           ${COLOR1}: ${WH}$ISP${NC}"
 echo -e " $COLOR1│$NC${WH} ❈ City          ${COLOR1}: ${WH}$CITY${NC}"
 echo -e " $COLOR1│$NC${WH} ❈ IP VPS        ${COLOR1}: ${WH}$MYIP${NC}"
-echo -e " $COLOR1│$NC${WH} ❈ CPU Usage     ${COLOR1}: ${WH}$cpu_usage%${NC}"
-echo -e " $COLOR1│$NC${WH} ❈ CPU Load      ${COLOR1}: ${WH}$cpu_load${NC}"
+echo -e " $COLOR1│$NC${WH} ❈ CPU Load      ${COLOR1}: ${WH}$cpu_load_percent%${NC}"
 echo -e " $COLOR1│$NC${WH} ❈ DOMAIN        ${COLOR1}: ${WH}$(cat /etc/xray/domain)"
 echo -e " $COLOR1│$NC${WH} ❈ NSDomain      ${COLOR1}: ${WH}$(cat /etc/xray/dns)"
 echo -e " $COLOR1╰══════════════════════════════════════════════════════════╯${NC}"
